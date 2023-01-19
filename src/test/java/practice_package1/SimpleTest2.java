4569ce8f257db0ba5f1f988cee0c6079768f7b42
Padmalochani <Padmalochani@DESKTOP-HPEUQME>
2023-01-19 13:00:48 +0530

    Created a new class

---
+++ src/test/java/practice_package1/SimpleTest2.java
@@ -0,0 +1,10 @@
+package practice_package1;
+
+import org.testng.annotations.Test;
+
+public class SimpleTest2 {
+@Test
+public void printMsg() {
+	System.out.println("Just a test message 2");
+}
+}
