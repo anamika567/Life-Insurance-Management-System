34cb9b542355af1b94bbcc297f9983a526f1f59b
hp <hp@DESKTOP-9BB5NPN>
2023-01-23 12:13:11 +0530

    new class

---
+++ src/test/java/practice_maven/Simple5Test.java
@@ -0,0 +1,12 @@
+package practice_maven;
+
+import org.testng.annotations.Test;
+
+public class Simple5Test {
+	
+	@Test
+	public void simple5() {
+		System.out.println("----tsetscript5----");
+	}
+
+}
